e00d06f0ca285ab2deae8d0823cc05da29904404
zayan175 <124209692+zayan175@users.noreply.github.com>
2024-01-11 04:33:27 +0500

    Update app.js

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    // origin : [process.env.FRONTEND_URL],
+    origin : [process.env.FRONTEND_URL],
     origin : "*" ,
-    // methods : ["GET","POST","PUT","DELETE"],
-    // credentials : true,
+    methods : ["GET","POST","PUT","DELETE"],
+    credentials : true,
 }))
 
 
